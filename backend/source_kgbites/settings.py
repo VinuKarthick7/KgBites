d6cfb1d84b795ff038a75c4e5ff7aefc8f742860
SRISHANTH P M <srishanth232007@gmail.com>
2025-09-23 14:34:21 +0530

    Updates in settings.py

--- backend/source_kgbites/settings.py
+++ backend/source_kgbites/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'source_kgbites.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'kgbites_db',                     
+        'USER': 'kgbites_user',                    
+        'PASSWORD': config('DB_PASSWORD'),               
+        'HOST': 'localhost',                        
+        'PORT': '5432',                             
     }
 }
 
@@ -128,5 +133,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 CORS_ORIGIN_ALLOW_ALL = True

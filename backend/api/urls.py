999b081ee75153d1abb714d2bd365ec953e7a9b8
VinuKarthick7 <vinukarthick6@gmail.com>
2025-09-23 21:43:10 +0530

    Api endpoints added

---
+++ backend/api/urls.py
@@ -0,0 +1,9 @@
+# api/urls.py
+
+from django.urls import path
+from .views import SignupView, LoginView
+
+urlpatterns = [
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+]
